6f667492c5b7e1125b267b1205fb4bef250d23c4
clchand23 <christina.l.chandra@gmail.com>
2022-02-07 14:53:24 -0500

    Install packages and set up renv

---
+++ install_stack.R
@@ -0,0 +1,33 @@
+# If private github packages are needed create a token by going to
+# https://github.com/settings/tokens
+# (Settings / Developer settings / Personal access token)
+# Select the `repo` scope
+
+# If your token is not stored as an environment variable
+## Sys.setenv(GITHUB_PAT = "your_token")
+# Do not share your token. It allows read/write access to your repositories
+
+# Packages installation
+if (file.exists("renv.lock")) {# Restore from a lockfile if it exists.
+
+  renv::restore(confirm = FALSE) # Do not ask for confirmation
+
+} else { # Otherwise install the packages and make a lockfile
+
+  # CRAN packages
+  install.packages(c(
+    "remotes",
+    "tergmLite",
+    "EpiModel"
+  ))
+
+  # Github packages
+  remotes::install_github(c(
+    "EpiModel/EpiModelHIV-p",
+    "EpiModel/EpiModelHIV-p@EDP",
+    "EpiModel/ARTnetData"
+  ), upgrade = FALSE)
+
+  # save the lockfile
+  renv::snapshot(type = "simple")
+}
