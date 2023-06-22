4bf5e9d43f6d8d2b2653775f033ae8a6588ecc8c
Adrien Le Guillou <git@aleguillou.org>
2023-06-22 09:01:30 +0200

    test calib hpc

--- R/utils-0_project_settings.R
+++ R/utils-0_project_settings.R
@@ -8,12 +8,12 @@ calib_dir <- "./data/intermediate/calibration/"
 scenarios_dir <- "./data/intermediate/scenarios/"
 
 # Information for the HPC workflows
-current_git_branch <- "main"
-mail_user <- "clchand@emory.edu" # or any other mail provider
+current_git_branch <- "alg_calib"
+mail_user <- "aleguil@emory.edu" # or any other mail provider
 
 # Relevant time steps for the simulation
-calibration_end    <- 364 * 10
+calibration_end    <- 364 * 45
 restart_time       <- calibration_end + 1
 prep_start         <- restart_time + 364 * 2
-intervention_start <- prep_start + 364 * 2
+intervention_start <- prep_start + 364 * 5
 intervention_end   <- intervention_start + 364 * 10
