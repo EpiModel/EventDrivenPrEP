dde0a8dd5f77bede0f624bd5b2091c08859b7807
Adrien Le Guillou <git@aleguillou.org>
2023-08-09 16:45:36 +0200

    restarted from okayish sims

--- R/22-restart_point_choose.R
+++ R/22-restart_point_choose.R
@@ -12,7 +12,7 @@ library("tidyr")
 # Choose the right context: "local" when choosing the restart point from local
 # runs, "hpc" otherwise. For "hpc", this
 #   assumes that you downloaded the "assessments_raw.rds" files from the HPC.
-context <- if (!exists("context")) c("local", "hpc")[1] else context
+context <-  c("local", "hpc")[2]
 source("R/utils-0_project_settings.R")
 source("R/utils-default_inputs.R", local = TRUE) # generate `path_to_restart`
 
