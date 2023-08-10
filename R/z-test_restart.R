3ef63146b07b4d4eda27e469ca7b644dc884ba3f
clchand23 <christina.l.chandra@gmail.com>
2023-08-10 10:43:11 -0400

    Create z-test_restart.R

---
+++ R/z-test_restart.R
@@ -0,0 +1,26 @@
+# Settings ---------------------------------------------------------------------
+context <- "hpc"
+source("R/utils-0_project_settings.R")
+
+# Run the simulations ----------------------------------------------------------
+library("EpiModelHIV")
+
+# Necessary files
+source("R/utils-default_inputs.R") # generate `path_to_restart`, `param`, `init`
+
+# Controls
+source("R/utils-targets.R")
+control <- control_msm(
+  start               = restart_time,
+  nsteps              = restart_time + 10,
+  nsims               = 1,
+  ncores              = 1,
+  initialize.FUN      = reinit_msm,
+  cumulative.edgelist = TRUE,
+  truncate.el.cuml    = 0,
+  .tracker.list       = calibration_trackers,
+  verbose             = TRUE
+)
+
+est <- readRDS(path_to_restart)
+netsim(est, param, init, control)
