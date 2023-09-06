0de60e2d2d256f93117eb052dda18dd06cf29970
Adrien Le Guillou <git@aleguillou.org>
2023-09-06 10:28:02 +0200

    update hpc config

--- R/utils-hpc_configs.R
+++ R/utils-hpc_configs.R
@@ -1,19 +1,19 @@
 # Must be sourced **AFTER** "./R/utils-0_project_settings.R"
 
-# on RSPH: cpu-per-task = 8, but only 7 at a time, mem-per-cpu = 5G
-hpc_configs <- EpiModelHPC::swf_configs_rsph(
-  partition = "preemptable",
-  r_version = "4.3.0",
-  mail_user = mail_user
-)
-#
-# # on mox: 20 sim per node, full mem, 24h
-# hpc_configs <- EpiModelHPC::swf_configs_hyak(
-#   hpc = "mox",
-#   partition = "ckpt",
-#   r_version = "4.1.2",
+# # on RSPH: cpu-per-task = 8, but only 7 at a time, mem-per-cpu = 5G
+# hpc_configs <- EpiModelHPC::swf_configs_rsph(
+#   partition = "preemptable",
+#   r_version = "4.3.0",
 #   mail_user = mail_user
 # )
+
+# on mox: 20 sim per node, full mem, 24h
+hpc_configs <- EpiModelHPC::swf_configs_hyak(
+  hpc = "mox",
+  partition = "ckpt",
+  r_version = "4.1.2",
+  mail_user = mail_user
+)
 
 # hpc_configs <- EpiModelHPC::swf_configs_hyak(
 #   hpc = "klone",
