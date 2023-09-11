a56e20ef7594716704971c2d44375a2cf9317638
Adrien Le Guillou <git@aleguillou.org>
2023-09-11 17:25:51 +0200

    up

--- R/z-test_autocalib.R
+++ R/z-test_autocalib.R
@@ -5,7 +5,7 @@ results <- readRDS("./results.rds")
 
 results |>
   group_by(.iteration) |>
-  summarize(across(starts_with("hiv.trans.scale_3"), list(
+  summarize(across(starts_with("prep.start.prob_2"), list(
         min = ~min(.x),
         max = ~max(.x)
         ))) |> print(n = 100)
@@ -14,15 +14,15 @@ results |>
 
 results |>
   filter(
-    abs(i.prev.dx.B - 0.33) < 0.02,
-    abs(i.prev.dx.H - 0.127) < 0.02,
-    abs(i.prev.dx.W - 0.09) < 0.02
+    abs(cc.prep.B - 0.33) < 0.02,
+    abs(cc.prep.H - 0.127) < 0.02,
+    abs(cc.prep.W - 0.09) < 0.02
   ) |>
   select(starts_with("hiv.trans.scale")) |>
   summary()
 
 results |>
-  select(starts_with("aids.off.tx"))|> summary()
+  select(starts_with("aids.off.tx")) |> summary()
 
 results |>
   group_by(.iteration) |>
